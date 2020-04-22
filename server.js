1e2b9ebf9de4727b7e28d5f61e9e340f8ce77441
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-22 08:33:15 -0700

    server.js backup

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const request = require('superagent');
 // Initiate database connection
 client.connect();
 
-// Initial Application Setup
+// Application Setup
 const app = express();
 const PORT = process.env.PORT;
 app.use(morgan('dev')); // http logging
